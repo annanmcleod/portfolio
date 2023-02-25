70df5a2301dab716f045709b8c11941a5d13e61b
annanmcleod <111473493+annanmcleod@users.noreply.github.com>
2023-02-25 14:16:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -116,22 +116,3 @@ document.addEventListener('mousemove', handleTouchMotion)
 window.addEventListener('DOMContentLoaded', init)
 
 
-// nav toggle @media
-
-$('#menu').click(function(){
-$(this).toggleClass('fa-times');
-$('header').toggleClass('toggle');
-});
-
-$(window).on('scroll load',function(){
-
-$('#menu').removeClass('fa-times');
-$('header').removeClass('toggle');
-
-if($(window).scrollTop() > 0){
-  $('.top').show();
-}else{
-  $('.top').hide();
-}
-
-});
